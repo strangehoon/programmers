a9a8479a230d9a0e14a77e5ef36d672386ca4a5e
sanghoonLee <strangehoon2@gmail.com>
2023-01-30 10:14:17 +0900

    [level 1] Title: 예산, Time: 0.35 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.java
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -2,14 +2,14 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] d, int budget) {
         int total = 0;
-        int count = 0;
+        int result = 0;
         Arrays.sort(d);
         for(int i = 0; i < d.length; i++) {
             if(d[i] + total <= budget) {
-                count ++;
+                result ++;
                 total += d[i];
             }
         }
-        return count;
+        return result;
     }
 }
