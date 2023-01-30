d6061c53788daa2b1c77517beb334dbfb7ed2b2a
sanghoonLee <strangehoon2@gmail.com>
2023-01-30 21:23:15 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 2.72 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -3,29 +3,40 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int divisor) {
         
-        // 리스트 생성
-        List<Integer> list= new ArrayList<>();
-        
-        // divisor로 나누어 떨어지는 값 리스트에 추가
-        for(int element : arr){
-            if(element % divisor == 0)
-                list.add(element);
-        }
-        
-        // divisor로 나누어 떨어지는 element가 하나도 없다면 리스트에 -1 담아 반환
-        if (list.isEmpty())
-            list.add(-1);
+        int[] intArray = Arrays.stream(arr).filter(i->i%divisor==0).sorted().toArray();
+        if (intArray.length == 0)
+            intArray = new int[]{-1};
 
-        int[] results = new int[list.size()];
-        
-        // 리스트에 있는 값들을 배열에 담기
-        for (int i = 0 ; i < list.size() ; i++) {
-            results[i] = list.get(i);
-        }
-        
-        // 오름차순 정렬
-        Arrays.sort(results);
-        return results;
+        return intArray;
             
     }
 }
+
+//orginal sol
+// class Solution2 {
+//     public int[] solution(int[] arr, int divisor) {
+//         // 리스트 생성
+//         List<Integer> list= new ArrayList<>();
+        
+//         // divisor로 나누어 떨어지는 값 리스트에 추가
+//         for(int element : arr){
+//             if(element % divisor == 0)
+//                 list.add(element);
+//         }
+        
+//         // divisor로 나누어 떨어지는 element가 하나도 없다면 리스트에 -1 담아 반환
+//         if (list.isEmpty())
+//             list.add(-1);
+
+//         int[] results = new int[list.size()];
+        
+//         // 리스트에 있는 값들을 배열에 담기
+//         for (int i = 0 ; i < list.size() ; i++) {
+//             results[i] = list.get(i);
+//         }
+        
+//         // 오름차순 정렬
+//         Arrays.sort(results);
+//         return results;    
+//     }
+// }
