d9aa932d9f16c9fb959fbcc3b43cfa6e5753394f
sanghoonLee <strangehoon2@gmail.com>
2023-01-31 11:10:32 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.41 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,4 +1,5 @@
 import java.util.*;
+/////////////////// orginal solution(HashMap 사용)
 class Solution {
     public int solution(String s) {
         String[] list = s.split("");
@@ -40,3 +41,19 @@ class Solution {
         return str != null && str.matches("[0-9.]+");
     }
 }
+
+/////////////////// better solution(replaceAll)
+
+// class Solution {
+//     public int solution(String s) {
+//         String s = "23four5six7";
+//         String[] digits = {"0","1","2","3","4","5","6","7","8","9"};
+//         String[] alphabets = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+//         for(int i=0; i<10; i++){
+//             s = s.replaceAll(alphabets[i],digits[i]);
+//         }
+//         return Integer.parseInt(s);
+//     }
+// }
+
