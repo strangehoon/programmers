71c95a8c179f8337cc663450e468c860bab217ad
sanghoonLee <strangehoon2@gmail.com>
2023-02-01 17:38:21 +0900

    [level 1] Title: 소수 찾기, Time: 200.88 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -1,14 +1,17 @@
 class Solution {
     public int solution(int n) {
+        //소수가 아닌 수 카운트
         int count = 0;
         for(int i = 2; i <= n; i++) {
             for(int j = 2; j <= Math.sqrt(i); j++){
+                // 
                 if(i % j == 0){
                     count ++;
                     break;
                 }
             }
         }
+        // 1은 소수가 아니므로 한개 빼주자
         int answer = n-1 - count;
         return answer;
     }
