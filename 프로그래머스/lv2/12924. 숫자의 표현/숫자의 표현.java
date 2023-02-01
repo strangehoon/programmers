011fe72e7b54fd3db583f6d9f9c2c1446d7ae2fd
sanghoonLee <strangehoon2@gmail.com>
2023-02-02 03:48:09 +0900

    [level 2] Title: 숫자의 표현, Time: 1.15 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n) {
+        // 자기 자신 카운트
+        int count = 1;
+        
+        // 절반만 검사
+        for(int i = n/2+1; i >1; i--){
+            int total = i;
+            while(total < n){
+                for(int j = i-1; j > 0; j--) {
+                    total += j;
+                    
+                    if (total== n){
+                        count++;
+                        break;
+                    }
+                    else if (total > n)
+                        break;
+                }
+                break;
+            }
+        }
+        return count;
+    }
+}
