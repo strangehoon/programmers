c7489cfee4deedf5bf4a27ffa639b7ac075efb9b
sanghoonLee <strangehoon2@gmail.com>
2023-02-02 03:20:13 +0900

    [level 1] Title: 폰켓몬, Time: 2.56 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -3,10 +3,12 @@ class Solution {
     public int solution(int[] nums) {
         int len = nums.length/2;
         HashSet<Integer> hashSet = new HashSet<>();
+        
         for(int element : nums)
             hashSet.add(element);
+        
         int setSize = hashSet.size();
-        int result = (setSize>=len) ? len:setSize;
-        return result;
+        
+        return (setSize>=len) ? len:setSize;
     }
 }
