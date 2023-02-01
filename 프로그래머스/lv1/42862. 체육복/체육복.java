57839fcaf49d9a41b3faaf3b9209b44497082f46
sanghoonLee <strangehoon2@gmail.com>
2023-02-02 02:56:22 +0900

    [level 1] Title: 체육복, Time: 4.32 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.stream.Collectors;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+
+        int result = n - lost.length;
+        List<Integer> lostList = Arrays.stream(lost).boxed().sorted().collect(Collectors.toList());
+        List<Integer> reserveList = Arrays.stream(reserve).boxed().sorted().collect(Collectors.toList());
+        for(int i = 0; i < lost.length; i++){
+            //여벌 체육복을 가져온 학생이 도난 당했을 경우
+            if(reserveList.contains(lost[i])){
+                reserveList.remove(Integer.valueOf(lost[i]));
+                lostList.remove(Integer.valueOf(lost[i]));
+                result++;
+            }
+        }
+        for(Integer x : lostList){
+            System.out.println("x = " + x);
+            int left = x -1;
+            int right = x + 1;
+
+            if(reserveList.contains(left)){
+                result ++;
+                reserveList.remove(Integer.valueOf(left));
+            }
+
+            else if (reserveList.contains(right)) {
+                result ++;
+                reserveList.remove(Integer.valueOf(right));
+            }
+
+        }
+        return result;
+    }
+}
