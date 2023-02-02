f1205b4f45aa8cb58d714a1818f874a628d12393
sanghoonLee <strangehoon2@gmail.com>
2023-02-02 14:11:55 +0900

    [level 1] Title: [1차] 비밀지도, Time: 5.38 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,4 +1,7 @@
 import java.util.*;
+
+/////////////////// original solution(10진수 -> 2진수 변환을 직접 구현함)
+
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         List<String> list1 = new ArrayList<>();
@@ -52,3 +55,22 @@ class Solution {
         return arr;
     }
 }
+
+/////////////////// better solution(toBinaryString 메서드, 비트연산자 활용)
+
+// class Solution {
+//   public String[] solution(int n, int[] arr1, int[] arr2) {
+//         String[] result = new String[n];
+//         for (int i = 0; i < n; i++) {
+//             result[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+//         }
+
+//         for (int i = 0; i < n; i++) {
+//             result[i] = String.format("%" + n + "s", result[i]);
+//             result[i] = result[i].replaceAll("1", "#");
+//             result[i] = result[i].replaceAll("0", " ");
+//         }
+
+//         return result;
+//     }
+// }
