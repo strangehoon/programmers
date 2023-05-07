f3f8d611648d5530b07a7783c22b6956f887a786
sanghoonLee <strangehoon2@gmail.com>
2023-05-07 23:36:50 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int price) {
+
+        if((price>=100000) && (price<300000)){
+            price *= 0.95;
+        } else if ((price >= 300000)&&(price < 500000)) {
+            price *= 0.90;
+        }
+        else if (price>=500000){
+            price *= 0.80;
+        }
+        return price;
+}
